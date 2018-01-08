fb86c583952258bff695b6822636ae01613348d7
Bart Meuris <bart.meuris@gmail.com>
2018-01-08 15:42:54 +0100

    Updated example

--- example/init/init.sh
+++ example/init/init.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
+[ -f "$(dirname $0)/env" ] && . "$(dirname $0)/env"
+
 # This script will automatically be called after installation
-export TITLE=example
 
 # Call default script to add authorized_keys file to all user directories
 $(dirname $0)/ssh_authorized_keys.sh || exit $?
